7d7bfa8a2fda28f2c3f57b402b78b06e525acec9
callumg2895 <callumgoodin@gmail.com>
2018-11-03 12:13:49 +0000

    Added new GetByProperty() method to the Repository class, allowing the user to retrieve a list of objects from the database matching the search criteria.

--- src/DataTrack.Core.Tests/RepositoryTest.cs
+++ src/DataTrack.Core.Tests/RepositoryTest.cs
@@ -78,6 +78,60 @@ namespace DataTrack.Core.Tests
         }
 
         [TestMethod]
+        public void TestRepository_ShouldReturnCorrectObjectForGetByPropertyType()
+        {
+            // Arrange
+            Book book1 = new Book() { ID = 1, AuthorId = 1, Title = "The Great Gatsby" };
+            Book book2 = new Book() { ID = 2, AuthorId = 1, Title = "The Beautiful and Damned" };
+            Author author = new Author()
+            {
+                ID = 1,
+                FirstName = "F.Scott",
+                LastName = "Fitzgerald",
+                Books = new List<Book>() { book1, book2 }
+            };
+
+            int createResult;
+            List<Book> bookReadResult;
+
+            //Act
+            stopwatch.Start();
+            createResult = Repository<Author>.Create(author);
+            stopwatch.Stop();
+            long authorInsertTime = stopwatch.ElapsedMilliseconds;
+
+            stopwatch.Restart();
+            bookReadResult = Repository<Book>.GetByProperty("title", Enums.RestrictionTypes.EqualTo, "The Great Gatsby");
+            stopwatch.Stop();
+            long bookReadTime = stopwatch.ElapsedMilliseconds;
+
+            stopwatch.Restart();
+            Repository<Book>.Delete(book1);
+            stopwatch.Stop();
+            long book1DeleteTime = stopwatch.ElapsedMilliseconds;
+
+            stopwatch.Restart();
+            Repository<Book>.Delete(book2);
+            stopwatch.Stop();
+            long book2DeleteTime = stopwatch.ElapsedMilliseconds;
+
+            stopwatch.Restart();
+            Repository<Author>.Delete(author);
+            stopwatch.Stop();
+            long authorDeleteTime = stopwatch.ElapsedMilliseconds;
+
+            Logger.Info(MethodBase.GetCurrentMethod(), $"Inserted 'Author' object in in {authorInsertTime}ms");
+            Logger.Info(MethodBase.GetCurrentMethod(), $"Read 'Book' object in {bookReadTime}ms");
+            Logger.Info(MethodBase.GetCurrentMethod(), $"Deleted 'Book' object in {book1DeleteTime}ms");
+            Logger.Info(MethodBase.GetCurrentMethod(), $"Deleted 'Book' object in {book2DeleteTime}ms");
+            Logger.Info(MethodBase.GetCurrentMethod(), $"Deleted 'Author' object in {authorDeleteTime}ms");
+
+            // Assert
+            Assert.AreEqual(createResult, 3);
+            Assert.IsTrue(BooksAreEqual(bookReadResult[0], book1));
+        }
+
+        [TestMethod]
         public void TestRepository_ShouldReadCorrectNumberOfChildItemsAfterInsertingObjectWithLongListOfChildren()
         {
             // Arrange
--- src/DataTrack.Core/Repository/Repository.cs
+++ src/DataTrack.Core/Repository/Repository.cs
@@ -6,54 +6,70 @@ using DataTrack.Core.SQL;
 using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Reflection;
+using DataTrack.Core.Enums;
 
 namespace DataTrack.Core.Repository
 {
     public static class Repository<TBase> where TBase : new()
     {
+        #region Create
+
         public static int Create(TBase item)
         {
-            Transaction<TBase> transaction = new Transaction<TBase>(
-                new List<QueryBuilder<TBase>>()
-                {
-                    new InsertQueryBuilder<TBase>(item)
-                });
+            Transaction<TBase> transaction = new Transaction<TBase>(new InsertQueryBuilder<TBase>(item));
 
             return (int)transaction.Execute()[0];
         }
 
+        #endregion
+
+        #region Read
+
         public static TBase GetByID(int id)
         {
-            Transaction<TBase> transaction = new Transaction<TBase>(
-                new List<QueryBuilder<TBase>>()
-                {
-                    new ReadQueryBuilder<TBase>(id)
-                });
+            Transaction<TBase> transaction = new Transaction<TBase>(new ReadQueryBuilder<TBase>(id));
 
             return (TBase)((List<TBase>)transaction.Execute()[0])[0];
         }
 
+        public static List<TBase> GetByProperty(string propName, RestrictionTypes restriction, object propValue)
+        {
+            Type propType = propValue.GetType();
+
+            ReadQueryBuilder<TBase> readBuilder = new ReadQueryBuilder<TBase>();
+
+            MethodInfo addRestriction;
+            addRestriction = readBuilder.GetType().GetMethod("AddRestriction", BindingFlags.Instance | BindingFlags.Public);
+            addRestriction.MakeGenericMethod(propType).Invoke(readBuilder, new object[] { propName, restriction, propValue });
+
+            Transaction<TBase> transaction = new Transaction<TBase>(readBuilder);
+
+            return (List<TBase>)transaction.Execute()[0];
+        }
+
+        #endregion
+
+        #region Update
+
         public static int Update(TBase item)
         {
-            Transaction<TBase> transaction = new Transaction<TBase>(
-                new List<QueryBuilder<TBase>>()
-                {
-                    new UpdateQueryBuilder<TBase>(item)
-                });
+            Transaction<TBase> transaction = new Transaction<TBase>(new UpdateQueryBuilder<TBase>(item));
 
             return (int)transaction.Execute()[0];
         }
 
+        #endregion
+
+        #region Delete
+
         public static void Delete(TBase item)
         {
-            Transaction<TBase> transaction = new Transaction<TBase>(
-                new List<QueryBuilder<TBase>>()
-                {
-                    new DeleteQueryBuilder<TBase>(item)
-                });
+            Transaction<TBase> transaction = new Transaction<TBase>(new DeleteQueryBuilder<TBase>(item));
 
             transaction.Execute();
         }
+
+        #endregion
     }
 }
