dcc0b546a47e1ac1d577cf47d33f2b777bdcc608
callumg2895 <callumgoodin@gmail.com>
2019-01-01 17:25:41 +0000

    Forgot to rename Mapping.cs file in last commit.

---
+++ src/DataTrack.Core/Util/DataStructures/Map.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataTrack.Core.Util.DataStructures
+{
+    public class Map<T1, T2>
+    {
+        private Dictionary<T1, T2> forward = new Dictionary<T1, T2>();
+        private Dictionary<T2, T1> reverse = new Dictionary<T2, T1>();
+
+        public Dictionary<T1, T2>.KeyCollection ForwardKeys { get => forward.Keys; }
+        public Dictionary<T2, T1>.KeyCollection ReverseKeys { get => reverse.Keys; }
+
+        public T2 this[T1 item]
+        {
+            get => forward.ContainsKey(item)
+                ? forward[item]
+                : throw new ArgumentOutOfRangeException($"Mapping does not contain key '{item.ToString()}'");
+            set
+            {
+                if (reverse.ContainsKey(value))
+                    throw new ArgumentOutOfRangeException($"Mapping already contains key '{value.ToString()}'");
+                else
+                {
+                    forward[item] = value;
+                    reverse[value] = item;
+                }
+            }
+        }
+
+        public T1 this[T2 item]
+        {
+            get => reverse.ContainsKey(item)
+                ? reverse[item]
+                : throw new ArgumentOutOfRangeException($"Mapping does not contain key '{item.ToString()}'");
+
+            set
+            {
+                if (forward.ContainsKey(value))
+                    throw new ArgumentOutOfRangeException($"Mapping already contains key '{value.ToString()}'");
+                else
+                {
+                    reverse[item] = value;
+                    forward[value] = item;
+                }
+            }
+        }
+
+        public bool ContainsKey(T1 item) => forward.ContainsKey(item);
+
+        public bool ContainsKey(T2 item) => reverse.ContainsKey(item);
+
+    }
+}
