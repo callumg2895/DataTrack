73abcff5518a20b811f2c12e42386768ea79121a
callumg2895 <callumgoodin@gmail.com>
2019-06-27 13:33:17 +0100

    Initial commit of LogTrack solution, which enables parsing of log files from DataTrack.

---
+++ src/LogTrack/LogTrack/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace LogTrack
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string projectName = "DataTrack";
+			string fileName = $"{projectName}Log_";
+			string filePath = $"{Path.GetPathRoot(Environment.SystemDirectory)}{projectName}";
+			string fileExtension = ".txt";
+			DateTime fileDate = DateTime.Now.Date;
+			string fileDateString = fileDate.ToShortDateString().Replace("/", "_");
+			int fileIndex = 0;
+
+			if (Directory.Exists(filePath))
+			{
+				using (StreamReader reader = File.OpenText($@"{filePath}\{fileDateString}_{fileName}{fileIndex}{fileExtension}"))
+				{
+					while (true)
+					{
+						Console.WriteLine(reader.ReadLine());
+
+						if (reader.EndOfStream)
+						{
+							break;
+						}
+
+					}
+				}
+			}
+		}
+	}
+}
