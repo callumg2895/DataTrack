4a1b01ac43184039b74ab5595ee680114c7c5d88
callumg2895 <callumgoodin@gmail.com>
2019-06-30 19:49:05 +0100

    Added a loading bar to display progress of log file parsing.

--- src/LogTrack/LogTrack/LogReader.cs
+++ src/LogTrack/LogTrack/LogReader.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace LogTrack
 {
 	public class LogReader
 	{
-		private List<LogStatement> logBuffer;
+		private volatile List<LogStatement> logBuffer;
 		private LogStats logStats;
 		private string filePath;
 		private string fileName;
+		private bool parsing;
+		private object parsingLock;
 
 		public LogReader(string path, string name)
 		{
@@ -19,6 +22,12 @@ namespace LogTrack
 			fileName = name;
 			logBuffer = new List<LogStatement>();
 			logStats = new LogStats();
+			parsingLock = new object();
+
+			lock (parsingLock)
+			{
+				parsing = false;
+			}
 		}
 
 		public List<LogStatement> Read()
@@ -30,6 +39,14 @@ namespace LogTrack
 
 			using (StreamReader reader = File.OpenText($"{filePath}/{fileName}"))
 			{
+				lock (parsingLock)
+				{
+					parsing = true;
+				}
+
+				Thread loadingBarThread = new Thread(new ThreadStart(LoadingBar));
+				loadingBarThread.Start();
+
 				while (true)
 				{
 					LogStatement statement = new LogStatement(reader.ReadLine());
@@ -46,6 +63,11 @@ namespace LogTrack
 
 					if (reader.EndOfStream)
 					{
+						lock (parsingLock)
+						{
+							parsing = false;
+						}
+
 						break;
 					}
 				}
@@ -57,6 +79,40 @@ namespace LogTrack
 		public LogStats ReadStats()
 		{
 			return logStats;
+		}
+
+		private void LoadingBar()
+		{
+			int maxLogSize = 10000;
+			int maxLoadingBarSize = 50;
+
+			while (true)
+			{
+				lock (parsingLock)
+				{
+					if (parsing)
+					{
+						Console.SetCursorPosition(1, 1);
+						Console.Write("Parsing log file: ");
+
+						int currentLogsParsed = Math.Max(logBuffer.Count(), 1);
+						int percentComplete = (int)Math.Round((currentLogsParsed / (decimal)maxLogSize) * maxLoadingBarSize);
+
+						for (int i = 0; i < percentComplete; i++)
+						{
+							Console.BackgroundColor = ConsoleColor.White;
+							Console.Write(" ");
+						}
+
+						Console.BackgroundColor = ConsoleColor.Black;
+						Console.WriteLine();
+					}
+					else
+					{
+						break;
+					}
+				}
+			}
 		}
 	}
 }
