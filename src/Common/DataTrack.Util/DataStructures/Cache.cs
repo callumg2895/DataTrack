2f3b10eb14ba4e2af88daf623acd129ad78458e8
callumg2895 <callumgoodin@gmail.com>
2019-08-19 17:27:52 +0100

    Created new Cache class such that there can be multiple types of cache in the future, which act like self culling dictionaries.

---
+++ src/Common/DataTrack.Util/DataStructures/Cache.cs
@@ -0,0 +1,165 @@
+﻿using DataTrack.Logging;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+
+namespace DataTrack.Util.DataStructures
+{
+	public class Cache<TKey, TValue>
+	{
+		private Dictionary<TKey, TValue> cacheDictionary;
+		private Dictionary<TKey, long> cacheAccessMapping;
+		private volatile bool shouldCull;
+		private volatile bool typeMappingInUse;
+
+		private readonly object typeMappingLock = new object();
+		private readonly object shouldCullLock = new object();
+		private readonly object typeMappingInUseLock = new object();
+
+		private readonly int maxCachedItems;
+
+		private Thread cullingThread;
+
+		public Cache(int cacheSizeLimit)
+		{
+			cacheDictionary = new Dictionary<TKey, TValue>();
+			cacheAccessMapping = new Dictionary<TKey, long>();
+
+			maxCachedItems = cacheSizeLimit;
+
+			shouldCull = true;
+			typeMappingInUse = true;
+
+			cullingThread = new Thread(new ThreadStart(Culling));
+			cullingThread.Start();
+		}
+
+		private bool ShouldCull()
+		{
+			lock (shouldCullLock)
+			{
+				return shouldCull;
+			}
+		}
+
+		private bool CullingInProgress()
+		{
+			lock (typeMappingInUseLock)
+			{
+				return typeMappingInUse;
+			}
+		}
+
+		private void EndExecution()
+		{
+			while (CullingInProgress())
+			{
+				continue;
+			}
+
+			lock (shouldCullLock)
+			{
+				shouldCull = false;
+			}
+		}
+
+		private void Culling()
+		{
+			while (!Logger.IsStarted())
+			{
+
+			}
+
+			while (ShouldCull())
+			{
+				Thread.Sleep(100);
+
+				int cacheSize = GetCurrentCacheSize();
+
+				Logger.Debug(MethodBase.GetCurrentMethod(), $"Beginning cache culling cycle - current cache size: {cacheSize} item(s)");
+
+				lock (typeMappingInUseLock)
+				{
+					typeMappingInUse = cacheSize > maxCachedItems;
+				}
+
+				if (cacheSize > maxCachedItems)
+				{
+					CullItem();
+				}
+			}
+		}
+
+		private int GetCurrentCacheSize()
+		{
+			lock (typeMappingLock)
+			{
+				return cacheDictionary.Count;
+			}
+		}
+
+		private void CullItem()
+		{
+			lock (typeMappingLock)
+			{
+				long oldestAccess = long.MaxValue;
+				TKey candidate = default;
+
+				foreach (TKey key in cacheAccessMapping.Keys)
+				{
+					if (cacheAccessMapping[key] >= oldestAccess)
+					{
+						continue;
+					}
+					else
+					{
+						oldestAccess = cacheAccessMapping[key];
+						candidate = key;
+					}
+				}
+
+				if (candidate != null && cacheDictionary.ContainsKey(candidate))
+				{
+					cacheDictionary.Remove(candidate);
+					cacheAccessMapping.Remove(candidate);
+					Logger.Info(MethodBase.GetCurrentMethod(), $"Culled values for key {candidate.ToString()}");
+				}
+			}
+		}
+
+		public void CacheItem(TKey key, TValue value)
+		{
+			lock (typeMappingLock)
+			{
+				Logger.Info(MethodBase.GetCurrentMethod(), $"Caching value {value.ToString()} for key '{key.ToString()}'");
+				cacheDictionary[key] = value;
+				cacheAccessMapping[key] = DateTime.UtcNow.ToFileTime();
+			}
+		}
+
+		public TValue RetrieveItem(TKey key)
+		{
+			TValue value = default(TValue);
+
+			lock (typeMappingLock)
+			{
+				if (cacheDictionary.ContainsKey(key) && cacheAccessMapping.ContainsKey(key))
+				{
+					value = cacheDictionary[key];
+					cacheAccessMapping[key] = DateTime.UtcNow.ToFileTime();
+
+					Logger.Info(MethodBase.GetCurrentMethod(), $"Retrieved value {value.ToString()} for key '{key.ToString()}' from cache");
+				}
+			}
+
+			return value;
+		}
+
+		public void Stop()
+		{
+			EndExecution();
+		}
+	}
+}
--- src/DataTrack/DataTrack.Core/Components/Mapping/MappingCache.cs
+++ src/DataTrack/DataTrack.Core/Components/Mapping/MappingCache.cs
@@ -1,4 +1,5 @@
 ﻿using DataTrack.Logging;
+using DataTrack.Util.DataStructures;
 using System;
 using System.Collections.Generic;
 using System.Reflection;
@@ -9,157 +10,26 @@ namespace DataTrack.Core.Components.Mapping
 {
 	public static class MappingCache
 	{
-
-		private static Dictionary<Type, EntityTable> TypeTableMapping;
-		private static Dictionary<Type, long> TypeAccessMapping;
-		private static volatile bool shouldCull;
-		private static volatile bool typeMappingInUse;
-
-		private static readonly object typeMappingLock = new object();
-		private static readonly object shouldCullLock = new object();
-		private static readonly object typeMappingInUseLock = new object();
-
-		private static int maxCachedItems = 10;
-
-		private static Thread cullingThread;
+		private static Cache<Type, EntityTable> mappingCache;
 
 		public static void Init(int cacheSizeLimit)
 		{
-			TypeTableMapping = new Dictionary<Type, EntityTable>();
-			TypeAccessMapping = new Dictionary<Type, long>();
-
-			maxCachedItems = cacheSizeLimit;
-
-			shouldCull = true;
-			typeMappingInUse = true;
-
-			cullingThread = new Thread(new ThreadStart(Culling));
-			cullingThread.Start();
-		}
-
-		private static bool ShouldCull()
-		{
-			lock (shouldCullLock)
-			{
-				return shouldCull;
-			}
-		}
-
-		private static bool CullingInProgress()
-		{
-			lock (typeMappingInUseLock)
-			{
-				return typeMappingInUse;
-			}
-		}
-
-		private static void EndExecution()
-		{
-			while (CullingInProgress())
-			{
-				continue;
-			}
-
-			lock (shouldCullLock)
-			{
-				shouldCull = false;
-			}
-		}
-
-		public static void Culling()
-		{
-			while (!Logger.IsStarted())
-			{
-
-			}
-
-			while (ShouldCull())
-			{
-				Thread.Sleep(100);
-
-				int cacheSize = GetCurrentCacheSize();
-
-				Logger.Debug(MethodBase.GetCurrentMethod(), $"Beginning cache culling cycle - current cache size: {cacheSize} item(s)");
-
-				lock (typeMappingInUseLock)
-				{
-					typeMappingInUse = cacheSize > maxCachedItems;
-				}
-
-				if (cacheSize > maxCachedItems)
-				{
-					CullItem();
-				}
-			}
-		}
-
-		private static int GetCurrentCacheSize()
-		{
-			lock (typeMappingLock)
-			{
-				return TypeTableMapping.Count;
-			}
-		}
-
-		private static void CullItem()
-		{
-			lock (typeMappingLock)
-			{
-				long oldestAccess = long.MaxValue;
-				Type? candidate = null;
-
-				foreach (Type key in TypeAccessMapping.Keys)
-				{
-					if (TypeAccessMapping[key] >= oldestAccess)
-					{
-						continue;
-					}
-					else
-					{
-						oldestAccess = TypeAccessMapping[key];
-						candidate = key;
-					}
-				}
-
-				if (candidate != null)
-				{
-					TypeTableMapping.Remove(candidate);
-					TypeAccessMapping.Remove(candidate);
-					Logger.Info(MethodBase.GetCurrentMethod(), $"Culled mapping for {candidate.Name}");
-				}
-			}
+			mappingCache = new Cache<Type, EntityTable>(cacheSizeLimit);
 		}
 
 		public static void CacheItem(Type type, EntityTable table)
 		{
-			lock (typeMappingLock)
-			{
-				Logger.Info(MethodBase.GetCurrentMethod(), $"Caching database mapping for Entity '{type.Name}'");
-				TypeTableMapping[type] = table;
-				TypeAccessMapping[type] = DateTime.UtcNow.ToFileTime();		
-			}
+			mappingCache.CacheItem(type, table);
 		}
 
-		public static EntityTable? RetrieveItem(Type type)
+		public static EntityTable RetrieveItem(Type type)
 		{
-			EntityTable? table = null;
-
-			lock (typeMappingLock)
-			{
-				if (TypeTableMapping.ContainsKey(type) && TypeAccessMapping.ContainsKey(type))
-				{
-					Logger.Info(MethodBase.GetCurrentMethod(), $"Loading Table object for '{type.Name}' entity from cache");
-					table = TypeTableMapping[type];
-					TypeAccessMapping[type] = DateTime.UtcNow.ToFileTime();
-				}				
-			}
-
-			return table;
+			return mappingCache.RetrieveItem(type);
 		}
 
 		public static void Stop()
 		{
-			EndExecution();
+			mappingCache.Stop();
 		}
 	}
 }
